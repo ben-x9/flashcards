7deeb2bb6f9e883bda9921db0be70538ca6f86c8
Ben Lambert <ben@joyxer.com>
2017-01-12 16:25:45 +0900

    import csx for css color help

--- app/pages/home.ts
+++ app/pages/home.ts
@@ -1,9 +1,12 @@
 import { h } from 'snabbdom/src/snabbdom';
 import { style } from 'typestyle';
-import { vertical, verticallySpaced, center, content, margin } from 'csstips';
+import { vertical, verticallySpaced, center, content, margin, padding } from 'csstips';
+import { lightgray } from 'csx';
 
 export const show = () =>
   h(`div.${style(margin(10), vertical, verticallySpaced(10), center)}`, [
     h(`div.${style(content)}`, 'hello world!'),
-    h(`div.${style(content)}`, 'hello world 2!'),
+    h(`div.${style(content, padding(10), {
+      backgroundColor: lightgray.toString(),
+    })}`, 'login'),
   ]);
