bb4a36f8c41eb5a7579d50430f01d7d898ba2808
Ben Lambert <ben@joyxer.com>
2017-01-23 14:00:39 +0900

    Show ticks on list item

--- app/components/list-item.ts
+++ app/components/list-item.ts
@@ -1,13 +1,16 @@
 import { div } from 'core/html';
 import { style } from 'typestyle';
-import { padding } from 'csstips';
+import { padding, horizontal } from 'csstips';
 import { white, black } from 'colors';
 import * as Card from 'components/card';
 import flippable from 'components/flippable';
+import { icon, tick } from 'styles';
+import { times } from 'lodash';
 
 export type Store = Card.Store;
 
 const cardClass = style(
+  horizontal,
   padding(10), {
   color: white,
   backgroundColor: black,
@@ -15,10 +18,28 @@ const cardClass = style(
   borderRadius: '3px',
 });
 
+const center = style({
+  position: 'absolute',
+  left: '50%',
+  transform: 'translateX(-50%)',
+});
+
+const right = style({
+  marginLeft: 'auto',
+});
+
+const textWithTicks = (text: string, numTicks: number, onclick: () => void,  styleName?: string) =>
+  div(styleName ? [cardClass, styleName] : cardClass, {on: {click: onclick}}, [
+    div(center, text),
+    div([right, style(horizontal, {minHeight: '18px'})], times(numTicks, () =>
+      div([style(icon), right], tick))),
+  ]);
+
 export const view = (store: Store, flipped: boolean, gap: number, onclick: () => void) =>
   flippable('list-item',
     'vert',
     flipped,
-    div(cardClass, {on: {click: onclick}}, store.front),
-    div([cardClass, style({right: gap, left: gap})], {on: {click: onclick}}, store.back),
+    textWithTicks(store.front, store.score, onclick),
+    textWithTicks(store.back, store.score, onclick,
+      style({right: gap, left: gap})),
   );
