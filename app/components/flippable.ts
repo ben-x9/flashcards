910247749000981a9b6aa582c13f9a6f10839ef5
Ben Lambert <ben@joyxer.com>
2017-01-23 11:42:34 +0900

    Small changes to flippable component
    This still is only working in Chrome, need to fix the HTML/CSS to work cross-browser

--- app/components/flippable.ts
+++ app/components/flippable.ts
@@ -5,23 +5,30 @@ import { VNode, VNodeData } from 'snabbdom/VNode';
 import { set } from 'core/common';
 
 const baseClass = style({
-  transition: 'transform 0.3s linear',
+  transition: 'transform 0.2s linear',
   backfaceVisibility: 'hidden',
 });
 
-const abs = style({position: 'absolute'});
+const frontClass = style({
+  $debugName: 'front',
+  zIndex: 2,
+});
+
+const abs = style({
+  position: 'absolute',
+});
 
 const flipHorizClass = style({transform: 'rotateY(180deg)'});
 const flipVertClass = style({transform: 'rotateX(180deg)'});
 
-export default (name: string, direction: 'horiz' | 'vert', flipped: boolean, front: VNode, back: VNode, styleClass: string | string[], data: VNodeData = {}) => {
+export default (name: string, direction: 'horiz' | 'vert', flipped: boolean, front: VNode, back: VNode, styleClass: string | string[] = '', data: VNodeData = {}) => {
   const flipClass = direction === 'horiz' ? flipHorizClass : flipVertClass;
   return div({name}, [
       style(vertical, content),
       ...(Array.isArray(styleClass) ? styleClass : [styleClass]),
     ], data, [
       set(front, {
-        sel: `${front.sel}.${baseClass}.${flipClass}`,
+        sel: `${front.sel}.${baseClass}.${frontClass}.${flipClass}`,
         data: set(front.data || {}, {
           class: set((front.data || {}).class || {}, {[flipClass]: flipped}),
         }),
